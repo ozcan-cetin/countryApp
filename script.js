4701f825a441c2c5169fb1359b1d2077b38b4384
özcan çetin <cetin445@gmail.com>
2022-06-25 00:32:21 +0300

    js v2

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ const select = document.querySelector('select');
 const flagsDiv = document.querySelector('.flags');
 const cardContainer = document.querySelector('.card-container');
 const regions = [];
-console.log(cardContainer);
+// console.log(flagsDiv);
 
 const fetchCountries = async ()=>{
     try {
@@ -13,7 +13,14 @@ const fetchCountries = async ()=>{
         const countries = await res.json();
         console.log(countries); // verilerin  geldiğini görmek için consolda yazmak lazım bu 3 satırı
         cardContainer.innerHTML = '';
-
+        countries.forEach((country)=>{
+            select.innerHTML += `<option value="${country.name.common}">${country.name.common}</option>`;
+            if(!regions.includes(country.region)){
+                regions.push(country.region);
+                regionButtonsDiv.innerHTML +=
+                `<button type="button" class="btn rounded-3 btn-primary m-2">${country.region}</button>`;
+            }
+        })
 
 
     } catch (error) {
